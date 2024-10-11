08d26795ad614e8f05f35aaeffd2ba109e5004de
Georgiy <gogapryst@gmail.com>
2024-10-11 20:00:50 +0300

    add search

--- base/urls.py
+++ base/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
 
-
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
